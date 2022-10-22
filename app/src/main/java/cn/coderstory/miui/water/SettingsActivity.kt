c947d624a98bc9dced4b3bc671e1c3e6835c3534
coderstory <coderstory@163.com>
2022-10-22 13:25:11 +0800

    删除miad的时机从打开软件转移到开关相应功能

--- app/src/main/java/cn/coderstory/miui/water/SettingsActivity.kt
+++ app/src/main/java/cn/coderstory/miui/water/SettingsActivity.kt
@@ -1,9 +1,11 @@
 package cn.coderstory.miui.water
 
+import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreference
 import com.topjohnwu.superuser.Shell
@@ -21,6 +23,7 @@ class SettingsActivity : FragmentActivity() {
         }
     }
 
+    @SuppressLint("WorldReadableFiles")
     private fun checkEdXposed() {
         try {
             getSharedPreferences("conf", MODE_WORLD_READABLE)
@@ -38,21 +41,19 @@ class SettingsActivity : FragmentActivity() {
             setPreferencesFromResource(R.xml.prefs, rootKey)
         }
 
-        override fun onActivityCreated(savedInstanceState: Bundle?) {
-            val findPreference = findPreference<SwitchPreference>("removeSplashAd2")
-            findPreference?.setOnPreferenceChangeListener { _, newValue ->
-                if (newValue as Boolean) {
+        override fun onPreferenceTreeClick(preference: Preference): Boolean {
+            if(preference.key == "removeSplashAd" ){
+                if((preference as SwitchPreference).isChecked){
                     Shell.su("rm -rf /storage/emulated/0/Android/data/com.miui.systemAdSolution/files/miad")
                         .exec()
                     Shell.su("touch /storage/emulated/0/Android/data/com.miui.systemAdSolution/files/miad")
                         .exec()
-                } else {
+                }else{
                     Shell.su("rm -rf /storage/emulated/0/Android/data/com.miui.systemAdSolution/files/miad")
                         .exec()
                 }
-                true
             }
-            super.onActivityCreated(savedInstanceState)
+            return super.onPreferenceTreeClick(preference)
         }
     }
 }
